2cbad74a5160cc85ae3155a8b79d56fb281fdc3d
rishabh <rishabh.jn1801@gmail.com>
2019-03-20 11:45:01 +0530

    added version.php

---
+++ version.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * iiitbsql question type version information.
+ *
+ * @package    qtype
+ * @subpackage iiitbxml
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'qtype_iiitbxml';
+$plugin->version   = 2012061700;
+
+$plugin->requires  = 2012061700;
+
+$plugin->maturity  = MATURITY_BETA;
+
