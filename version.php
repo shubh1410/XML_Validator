add4522dc54acc031463f6cff047cf124b607f03
rishabh <rishabh.jn1801@gmail.com>
2019-03-20 11:55:27 +0530

    changed version.hpp

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 
 /**
- * iiitbsql question type version information.
+ * iiitbxml question type version information.
  *
  * @package    qtype
  * @subpackage iiitbxml
@@ -12,6 +12,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_iiitbxml';
 $plugin->version   = 2012061700;
+//this means 17th june 2012, will change later accordingly
 
 $plugin->requires  = 2012061700;
 
