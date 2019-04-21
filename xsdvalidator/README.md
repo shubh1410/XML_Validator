c73238896c60e4003f7b1eb0801591ea99eae3e9
Sagar2149 <sagarjhunjhunwala@gmail.com>
2019-04-21 15:49:59 +0530

    xsd validator with dependency pmatch

---
+++ xsdvalidator/README.md
@@ -0,0 +1,42 @@
+# Reverse pattern match question type [![Build Status](https://travis-ci.org/timhunt/moodle-qtype_xsdvalidator.svg?branch=master)](https://travis-ci.org/timhunt/moodle-qtype_xsdvalidator)
+
+The pattern match question type allows the teacher to grade sentences entered by
+the student using the pattern-match language. This question type does it backwards.
+The teacher provides some sentences, and says whether or not they should be matched,
+and the student has to come up with a pattern-match expression that does that.
+This question type is intended to be used to help people learn pattern-match
+syntax.
+
+The pattern-match language is documented at
+http://docs.moodle.org/dev/The_OU_PMatch_algorithm
+
+
+## Acknowledgements
+
+This question type was created by Tim Hunt just for fun.
+
+
+## Installation
+
+This plugin should be compatible with Moodle 3.4+.
+
+### Install from the plugins database
+
+Install from the Moodle plugins database
+* https://moodle.org/plugins/qtype_pmatch
+* https://moodle.org/plugins/editor_ousupsub
+* https://moodle.org/plugins/qtype_xsdvalidator
+
+### Install using git
+
+To install using git, type these commands in the root of your Moodle install
+
+    git clone https://github.com/moodleou/moodle-qtype_pmatch.git question/type/pmatch
+    echo /question/type/pmatch/ >> .git/info/exclude
+    git clone https://github.com/moodleou/moodle-editor_ousupsub.git lib/editor/ousupsub
+    echo /lib/editor/ousupsub/ >> .git/info/exclude
+    git clone https://github.com/timhunt/moodle-qtype_xsdvalidator.git question/type/xsdvalidator
+    echo '/question/type/xsdvalidator' >> .git/info/exclude
+
+Then run the moodle update process
+Site administration > Notifications
---
+++ xsdvalidator/lang/en/qtype_xsdvalidator.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_xsdvalidator', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   qtype_xsdvalidator
+ * @copyright 2013 Tim Hunt
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addmoresentences'] = 'Blanks for {no} more sentences';
+$string['defaultquestiontext'] = 'Please enter a valid XSD which matches, or not, the given example xml.';
+$string['doesmatch'] = 'Does Validate';
+$string['doesnotmatchex'] = 'Does not Validate \'{$a}\'';
+$string['dontmatchx'] = 'Dont\'s validate: {$a}';
+$string['examplesentencen'] = 'Example sentence {$a}';
+$string['examplesentencerequired'] = 'You must enter at least one example xml';
+$string['examplesmustbeunique'] = 'Each example xml must be different.';
+$string['matchx'] = 'Validate: {$a}';
+$string['matchesx'] = 'Does not validate \'{$a}\'';
+$string['pluginname'] = 'xsdvalidator';
+$string['pluginname_help'] = 'With the xsdvalidator question type, the xsd typed by the student is graded using xml test cases entered by the teacher.';
+$string['pluginname_link'] = 'question/type/xsdvalidator';
+$string['pluginnameadding'] = 'Adding a xsd validator question';
+$string['pluginnameediting'] = 'Editing a xsd validator question';
+$string['pluginnamesummary'] = 'With the xsdvalidator question type, the xsd typed by the student is graded using xml test cases entered by the teacher.';
+$string['sentence'] = 'XML';
+$string['shouldmatch'] = 'Should validate';
+$string['shouldnotmatch'] = 'Should not validate';
