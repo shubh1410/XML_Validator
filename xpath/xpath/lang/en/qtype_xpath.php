f621046db6b68301bc24a5d121c287b1ed702a8c
Prashantsangtani <prashant.sangtani@gmail.com>
2019-04-28 08:59:41 +0530

    Xpath removed xpath shows xml after attempt quiz

---
+++ xpath/xpath/lang/en/qtype_xpath.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Strings for component 'qtype_xpath', language 'en'
+ *
+ * @package    qtype
+ * @subpackage xpath
+ */
+
+$string['addmoreanswerblanks'] = 'Blanks for {no} More Answers';
+$string['answer'] = 'Answer: {$a}';
+$string['answermustbegiven'] = 'You must enter an answer if there is a grade or feedback.';
+$string['answerno'] = 'File ';
+$string['caseno'] = 'No, case is unimportant';
+$string['casesensitive'] = 'Case sensitivity';
+$string['caseyes'] = 'Yes, case must match';
+$string['correctansweris'] = 'The correct answer is: {$a}.';
+$string['correctanswers'] = 'Correct answers';
+$string['filloutoneanswer'] = 'You must provide at least one possible answer. Answers left blank will not be used. The first matching answer will be used to determine the score and feedback. Please do not put ; after the query.';
+$string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
+$string['pleaseenterananswer'] = 'Please enter an answer. Please do not put ; after the query';
+$string['pluginname'] = 'xpath';
+$string['pluginname_help'] = 'In response to a question the respondent types a MySql query. There may be several possible correct answers, each with a different grade. If the "Case sensitive" option is selected, then you can have different scores.';
+$string['pluginname_link'] = 'question/type/xpath';
+$string['pluginnameadding'] = 'Adding a xpath question';
+$string['pluginnameediting'] = 'Editing a xpath question';
+$string['pluginnamesummary'] = 'Allows a response xquery that is graded by comparing against various model queries.';
+$string['db_hostname'] = 'localhost';
+$string['db_username'] = 'readuser';
+$string['db_password'] = 'thanks123';
